cea9b9556d4f1cc81294a63037270b4a9ab40521
johnsalzy <salzyjohn@gmail.com>
2019-08-07 22:43:52 -0400

    added 2 utilites
    One utilite to check if a user is logged in and another to check their access level

--- utilities.py
+++ utilities.py
@@ -23,3 +23,25 @@ def verify_password(stored_password, provided_password):
                                   100000)
     pwdhash = binascii.hexlify(pwdhash).decode('ascii')
     return pwdhash == stored_password
+
+
+def checkUserLoggedIn(username):
+    """
+    Will check the database to see if the user it logged in
+    """
+    #Check user logged from database
+    # If the have a token, they are logged in
+    user_logged_in = True
+    return user_logged_in
+
+
+def checkUserAccessLevel(username):
+    """
+    Given a username will check the users
+    access level from the database
+    Note: Check README for different 
+    types of access levels
+    """
+    #Check user access level from database
+    access_level = 'Basic'
+    return access_level
